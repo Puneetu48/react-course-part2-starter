28c8ea3e8de9b76f69b7528aa74e52c7f4a1c9e4
Puneet Upadhyay <Puneetu48@users.noreply.github.com>
2023-11-06 21:00:12 -0600

    Example for unnecassary renders

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -5,7 +5,8 @@ import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks } = useContext(TasksContext);
-  const { counter } = useCounterStore();
+  console.log("render navbar");
+  const counter = useCounterStore((s) => s.counter);
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{counter}</span>
